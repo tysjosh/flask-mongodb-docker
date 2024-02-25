960108ae003ed29d0a55bdf8f7e21c43fe4ecd1c
Olatayo Olukotun <olukotunjosh@gmail.com>
2024-02-25 09:02:33 +0100

    initial commit

---
+++ services/web/project/__init__.py
@@ -0,0 +1,185 @@
+"""
+cocktailapi - A small API for managing cocktail recipes.
+"""
+
+from datetime import datetime
+import os
+
+import flask
+from flask import Flask, request, url_for, jsonify, session, abort, redirect
+from authlib.integrations.flask_client import OAuth
+from flask_pymongo import PyMongo
+from pymongo.errors import DuplicateKeyError
+from pymongo.collection import Collection, ReturnDocument
+
+from .models import Cocktail
+from .objectid import PydanticObjectId
+
+# Configure Flask & Flask-PyMongo:
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+pymongo = PyMongo(app)
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_SECRET_KEY"),
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    authorize_params=None,
+    access_token_params=None,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+)
+
+# Get a reference to the recipes collection.
+# Uses a type-hint, so that your IDE knows what's happening!
+recipes: Collection = pymongo.db.recipes
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "user" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
+
+@app.route('/')
+def index():
+    if 'user' in session:
+        me = google.get('userinfo')
+        return jsonify({'data': me.data})
+    return 'Hello! Log in with your Google account: <a href="/login">Log in</a>'
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/login/authorized')
+def authorized():
+    token = google.authorize_access_token()
+    session['user'] = token['userinfo']
+    # You can perform registration process using this information if needed.
+
+    return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+@oauth.tokengetter
+def get_google_oauth_token():
+    return session.get('user')
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    """
+    An error-handler to ensure that 404 errors are returned as JSON.
+    """
+    return jsonify(error=str(e)), 404
+
+
+@app.errorhandler(DuplicateKeyError)
+def resource_not_found(e):
+    """
+    An error-handler to ensure that MongoDB duplicate key errors are returned as JSON.
+    """
+    return jsonify(error=f"Duplicate key error."), 400
+
+@login_is_required
+@app.route("/cocktails/")
+def list_cocktails():
+    """
+    GET a list of cocktail recipes.
+
+    The results are paginated using the `page` parameter.
+    """
+
+    page = int(request.args.get("page", 1))
+    per_page = 10  # A const value.
+
+    # For pagination, it's necessary to sort by name,
+    # then skip the number of docs that earlier pages would have displayed,
+    # and then to limit to the fixed page size, ``per_page``.
+    cursor = recipes.find().sort("name").skip(per_page * (page - 1)).limit(per_page)
+
+    cocktail_count = recipes.count_documents({})
+
+    links = {
+        "self": {"href": url_for(".list_cocktails", page=page, _external=True)},
+        "last": {
+            "href": url_for(
+                ".list_cocktails", page=(cocktail_count // per_page) + 1, _external=True
+            )
+        },
+    }
+    # Add a 'prev' link if it's not on the first page:
+    if page > 1:
+        links["prev"] = {
+            "href": url_for(".list_cocktails", page=page - 1, _external=True)
+        }
+    # Add a 'next' link if it's not on the last page:
+    if page - 1 < cocktail_count // per_page:
+        links["next"] = {
+            "href": url_for(".list_cocktails", page=page + 1, _external=True)
+        }
+
+    return {
+        "recipes": [Cocktail(**doc).to_json() for doc in cursor],
+        "_links": links,
+    }
+
+@login_is_required
+@app.route("/cocktails/", methods=["POST"])
+def new_cocktail():
+    raw_cocktail = request.get_json()
+    raw_cocktail["date_added"] = datetime.utcnow()
+
+    cocktail = Cocktail(**raw_cocktail)
+    insert_result = recipes.insert_one(cocktail.to_bson())
+    cocktail.id = PydanticObjectId(str(insert_result.inserted_id))
+    print(cocktail)
+
+    return cocktail.to_json()
+
+@login_is_required
+@app.route("/cocktails/<string:slug>", methods=["GET"])
+def get_cocktail(slug):
+    recipe = recipes.find_one_or_404({"slug": slug})
+    return Cocktail(**recipe).to_json()
+
+@login_is_required
+@app.route("/cocktails/<string:slug>", methods=["PUT"])
+def update_cocktail(slug):
+    cocktail = Cocktail(**request.get_json())
+    cocktail.date_updated = datetime.utcnow()
+    updated_doc = recipes.find_one_and_update(
+        {"slug": slug},
+        {"$set": cocktail.to_bson()},
+        return_document=ReturnDocument.AFTER,
+    )
+    if updated_doc:
+        return Cocktail(**updated_doc).to_json()
+    else:
+        flask.abort(404, "Cocktail not found")
+
+@login_is_required
+@app.route("/cocktails/<string:slug>", methods=["DELETE"])
+def delete_cocktail(slug):
+    deleted_cocktail = recipes.find_one_and_delete(
+        {"slug": slug},
+    )
+    if deleted_cocktail:
+        return Cocktail(**deleted_cocktail).to_json()
+    else:
+        flask.abort(404, "Cocktail not found")
